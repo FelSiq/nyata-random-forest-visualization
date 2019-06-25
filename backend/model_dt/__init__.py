09b425f62b26bea5d8f2393fda84fa5007d64a86
felsiq <felipe.siqueira@usp.br>
2019-06-25 18:54:58 -0300

    Updated backend

--- backend/model_dt/__init__.py
+++ backend/model_dt/__init__.py
@@ -1,15 +1,20 @@
 import flask
 import flask_cors
+import flask_restful
 
 from . import model_dt
+
+class DecisionTree(flask_restful.Resource):
+    def get(self):
+        return model_dt.serialize_decision_tree(model_dt.get_toy_model())
 
 def create_app():
     """DT visualization application factory."""
     app = flask.Flask(__name__, instance_relative_config=True)
-    flask_cors.CORS(app)
 
-    @app.route("/dt-visualization")
-    def visualization():
-        return model_dt.serialize_decision_tree(model_dt.get_toy_model())
+    flask_cors.CORS(app)
+    api = flask_restful.Api(app)
+
+    api.add_resource(DecisionTree, '/dt-visualization')
 
     return app
