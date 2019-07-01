d824483aa1e0239848dec0f39ed75959b61b0c54
felsiq <felipe.siqueira@usp.br>
2019-07-01 17:16:37 -0300

    Updated app module imports

--- frontend/model-dt/src/app/app.module.ts
+++ frontend/model-dt/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { DtModelComponent } from './dt-model/dt-model.component';
 
 import { D3Service } from 'd3-ng2-service';
+import { InstanceBarComponent } from './instance-bar/instance-bar.component';
+import { TreeDisplayerComponent } from './dt-model/tree-displayer/tree-displayer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DtModelComponent,
+    InstanceBarComponent,
+    TreeDisplayerComponent,
   ],
   imports: [
     BrowserModule,
