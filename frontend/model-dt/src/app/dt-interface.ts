f1c3d24669d52f87cf24716d0e8d4dc555b3b3d1
FelSiq <felipe.siqueira@usp.br>
2019-07-09 16:51:52 -0300

    Updated frontend

--- frontend/model-dt/src/app/dt-interface.ts
+++ frontend/model-dt/src/app/dt-interface.ts
@@ -1,21 +1,68 @@
+export interface TreeInterface {
+  capacity: number;
+  children_left: number[];
+  children_right: number[];
+  feature: number[];
+  impurity: number[];
+  max_depth: number;
+  max_n_classes: number;
+  n_classes: number[];
+  n_features: number;
+  n_node_samples: number[];
+  n_outputs: number;
+  node_count: number;
+  threshold: number[];
+  value: Array<any>;
+  weighted_n_node_samples: number[];
+}
+
+export interface BaseEstimator {
+  class_weight: number[];
+  criterion: string;
+  max_depth: number;
+  max_features: number;
+  max_leaf_nodes: number;
+  min_impurity_decrease: number;
+  min_impurity_split: number;
+  min_samples_leaf: number;
+  min_samples_split: number;
+  min_weight_fraction_leaf: number;
+  presort: boolean;
+  random_state: number;
+  splitter: string;
+}
+
 export interface DTInterface {
   criterion: string;
-  splitter: string;
-  max_depth: string;
   min_samples_split: string;
   min_samples_leaf: string;
   min_weight_fraction_leaf: string;
   max_features: string;
-  random_state: string;
   max_leaf_nodes: string;
-  min_impurity_decrease: string;
-  min_impurity_split: string;
   class_weight: string;
-  presort: string;
-  n_features_: string;
-  n_outputs_: string;
-  classes_: string;
-  n_classes_: string;
-  max_features_: string;
-  tree_: string;
+
+  max_depth: number;
+  random_state: number;
+  min_impurity_decrease: number;
+  min_impurity_split: number;
+  n_features_: number;
+  n_outputs_: number;
+  classes_: Array<number | string>;
+  n_classes_: number;
+
+  tree_?: TreeInterface;
+  estimators_?: DTInterface[];
+
+  presort?: boolean;
+  splitter?: string;
+  warm_start?: boolean;
+  n_estimators?: string;
+  estimator_params?: any[];
+  oob_score?: boolean;
+  verbose?: boolean;
+  base_estimator?: BaseEstimator;
+  base_estimator_?: BaseEstimator;
+  bootstrap?: boolean;
+  n_jobs?: number;
+  max_features_?: number;
 }
--- frontend/model-dt/src/app/dt-model/tree-displayer/tree-d3-model/tree-d3-model.component.ts
+++ frontend/model-dt/src/app/dt-model/tree-displayer/tree-d3-model/tree-d3-model.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import * as d3 from 'd3';
 
-import { DTInterface } from '../../../dt-interface';
+import { TreeInterface } from '../../../dt-interface';
 
 @Component({
   selector: 'app-tree-d3-model',
@@ -8,51 +9,23 @@ import { DTInterface } from '../../../dt-interface';
   styleUrls: ['./tree-d3-model.component.css']
 })
 export class TreeD3ModelComponent implements OnInit, OnChanges {
-  @Input() treeModel: DTInterface;
-  private modelContainer: ElementRef;
-  private testData;
+  @Input() treeNodes: TreeInterface[];
   public d3Model;
+  public chosenTree: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.testData = {
-      "name": "Eve",
-      "children": [
-        {
-          "name": "Cain"
-        },
-        {
-          "name": "Seth",
-          "children": [
-            {
-              "name": "Enos"
-            },
-            {
-              "name": "Noam"
-            }
-          ]
-        },
-        {
-          "name": "Abel"
-        },
-        {
-          "name": "Awan",
-          "children": [
-            {
-              "name": "Enoch"
-            }
-          ]
-        },
-        {
-          "name": "Azura"
-        }
-      ]
-    };
+    this.chosenTree = 0;
+  }
+
+  updateChosenTree(treeId: number): void {
+    this.chosenTree = +treeId;
+    console.log(this.chosenTree, treeId);
   }
 
   ngOnChanges(): void {
-    if (!this.treeModel) {
+    if (!this.treeNodes) {
       return;
     }
 
@@ -60,64 +33,5 @@ export class TreeD3ModelComponent implements OnInit, OnChanges {
   }
 
   private createTree(): void {
-    const width = 932;
-
-    const tree = data => {
-      const root = d3.hierarchy(data);
-      root.dx = 10;
-      root.dy = width / (root.height + 1);
-      return d3.tree().nodeSize([root.dx, root.dy])(root);
-    }
-
-    const root = tree(this.testData);
-  
-    let x0 = Infinity;
-    let x1 = -x0;
-    root.each(d => {
-      if (d.x > x1) x1 = d.x;
-      if (d.x < x0) x0 = d.x;
-    });
-  
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, x1 - x0 + root.dx * 2]);
-    
-    const g = svg.append("g")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
-      
-    const link = g.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
-    .selectAll("path")
-      .data(root.links())
-      .join("path")
-        .attr("d", d3.linkHorizontal()
-            .x(d => d.y)
-            .y(d => d.x));
-    
-    const node = g.append("g")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-width", 3)
-      .selectAll("g")
-      .data(root.descendants())
-      .join("g")
-        .attr("transform", d => `translate(${d.y},${d.x})`);
-  
-    node.append("circle")
-        .attr("fill", d => d.children ? "#555" : "#999")
-        .attr("r", 2.5);
-  
-    node.append("text")
-        .attr("dy", "0.31em")
-        .attr("x", d => d.children ? -6 : 6)
-        .attr("text-anchor", d => d.children ? "end" : "start")
-        .text(d => d.data.name)
-      .clone(true).lower()
-        .attr("stroke", "white");
-    
-    this.d3Model = svg.node();
   }
 }
