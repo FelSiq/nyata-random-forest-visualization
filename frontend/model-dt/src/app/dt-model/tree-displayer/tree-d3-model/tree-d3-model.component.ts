43076fbbb5ee3b4f8ced96e70cfad360ad0c24e0
FelSiq <felipe.siqueira@usp.br>
2019-07-20 17:00:36 -0300

    Zoom demo

--- frontend/model-dt/src/app/dt-model/tree-displayer/tree-d3-model/tree-d3-model.component.ts
+++ frontend/model-dt/src/app/dt-model/tree-displayer/tree-d3-model/tree-d3-model.component.ts
@@ -15,25 +15,6 @@ export class TreeD3ModelComponent implements OnInit {
   @Input() treeNodes: DTInterface[];
   chosenTree: string | number;
 
-  /**
-  To read:
-  https://github.com/d3/d3-zoom
-  https://bl.ocks.org/mbostock/3127661b6f13f9316be745e77fdfb084
-
-  https://github.com/d3/d3-drag
-  https://bl.ocks.org/mbostock/ad70335eeef6d167bc36fd3c04378048
-
-  https://github.com/d3/d3-force
-  https://bl.ocks.org/mbostock/2675ff61ea5e063ede2b5d63c08020c7
-  https://bl.ocks.org/mbostock/f584aa36df54c451c94a9d0798caed35
-
-  https://github.com/d3/d3-brush
-  https://bl.ocks.org/mbostock/0d20834e3d5a46138752f86b9b79727e
-
-  https://bl.ocks.org/mbostock/3680999
-
-  */
-
   private svg: any;
   private links: any;
   private nodes: any;
@@ -49,18 +30,33 @@ export class TreeD3ModelComponent implements OnInit {
 
   changesHandler(): void {
     this.initSvg();
-    this.cleanSvg();
     this.createTree();
   }
 
   private initSvg() {
     this.svg = d3.select('svg');
 
+    this.cleanSvg();
+
+    this.links = this.svg.append('g')
+        .attr('class', 'group-links');
+
+    this.nodes = this.svg.append('g')
+        .attr('class', 'group-nodes')
+
+    this.svg.call(d3Zoom.zoom()
+      .scaleExtent([0.5, 3])
+      .on('zoom', () => {
+        this.svg
+          .attr('transform', d3.event.transform);
+      }));
+
     this.width = (
         +this.svg.attr('width') ?
         +this.svg.attr('width') :
         this.eleRef.nativeElement.offsetWidth
     );
+
     this.height = (
         +this.svg.attr('height') ?
         +this.svg.attr('height') :
@@ -69,11 +65,16 @@ export class TreeD3ModelComponent implements OnInit {
   }
 
   private cleanSvg(): void {
-    this.svg.selectAll('*').remove();
+    if (this.nodes) {
+      this.nodes.selectAll('*').remove();
+    }
+
+    if (this.links) {
+      this.links.selectAll('*').remove();
+    }
   }
 
   private createTree(): void {
-
     if (!this.svg || !this.treeNodes) {
       return;
     }
@@ -87,21 +88,77 @@ export class TreeD3ModelComponent implements OnInit {
 
     const curTree = curTreeNodes.tree_.value as TreeInterface;
 
-    this.links = this.svg.append('g')
-        .attr('class', 'group-links');
-
-    this.nodes = this.svg.append('g')
-        .attr('class', 'group-nodes')
-
     this.buildNode(
         curTree,
         0,
-        4 + curTree.max_depth,
+        10,
         this.width / 2,
         this.width / 4,
         0.02 * this.height,
         0.98 * this.height / (1 + curTree.max_depth));
 
+  }
+
+  private generateLink(nodeAId: number,
+                       nodeBId: number,
+                       x1: number,
+                       x2: number,
+                       y1: number,
+                       y2: number): void {
+
+    this.links.append('line')
+      .attr('class', 'link')
+      .attr('id', 'link-' + nodeAId + '-' + nodeBId)
+      .attr('x1', x1)
+      .attr('x2', x2)
+      .attr('y1', y1)
+      .attr('y2', y2)
+      .style('stroke', 'rgb(64,64,64)')
+      .style('stroke-width', 2);
+  }
+
+  private connectNodes(nodeAId: number, nodeBId: number): void {
+    const nodeA = this.nodes.select("#node-" + nodeAId);
+    const nodeB = this.nodes.select("#node-" + nodeBId);
+
+    this.generateLink(
+      nodeAId,
+      nodeBId,
+      nodeA.attr('cx'),
+      nodeB.attr('cx'),
+      nodeA.attr('cy'),
+      nodeB.attr('cy'));
+  }
+
+  private generateNode(nodeId: number,
+                       cx: number,
+                       cy: number,
+                       radius: number): void {
+    this.nodes.append('circle')
+      .attr('class', 'node')
+      .attr('id', 'node-' + nodeId) 
+      .attr('stroke', 'gray')
+      .attr('fill', 'white')
+      .attr('cx', cx)
+      .attr('cy', cy)
+      .attr('r', radius)
+      .call(d3Drag.drag()
+        .on('start', function() {
+          d3.select(this)
+            .raise()
+            .classed('active', true)
+            .attr('r', 2 * radius);
+        })
+        .on('end', function() {
+          d3.select(this)
+            .classed('active', false)
+            .attr('r', radius);
+        })
+        .on('drag', function() {
+          d3.select(this)
+            .attr('cx', d3.event.x)
+            .attr('cy', d3.event.y);
+        }));
   }
 
   private buildNode(
@@ -113,69 +170,41 @@ export class TreeD3ModelComponent implements OnInit {
         cy: number,
         cyDelta: number): void {
 
-      const sonLeft = curTree.children_left[nodeId];
-      const sonRight = curTree.children_right[nodeId];
+      const sonLeftId = +curTree.children_left[nodeId];
+      const sonRightId = +curTree.children_right[nodeId];
 
-      this.nodes.append('circle')
-        .attr('class', 'node')
-        .attr('id', 'node-' + nodeId) 
-        .attr('cx', cx)
-        .attr('cy', cy)
-        .attr('r', radius)
-        .call(d3Drag.drag()
-          .on('start', function() {
-            d3.select(this)
-              .raise()
-              .classed('active', true);
-          })
-          .on('end', function() {
-            d3.select(this)
-              .classed('active', false);
-          })
-          .on('drag', function() {
-            d3.select(this)
-              .attr('cx', d3.event.x)
-              .attr('cy', d3.event.y);
-          }));
+      this.generateNode(nodeId, cx, cy, radius);
 
-      if (sonLeft >= 0 && sonLeft < curTree.capacity) {
-        this.links.append('line')
-          .attr('class', 'link')
-          .attr('x1', cx)
-          .attr('y1', cy)
-          .attr('x2', cx - cxDelta)
-          .attr('y2', cy + cyDelta)
-          .style('stroke', 'rgb(64,64,64)')
-          .style('stroke-width', 4);
+      if (sonLeftId >= 0 && sonLeftId < curTree.capacity) {
+        const cxSonLeft = cx - cxDelta;
+        const cySonLeft = cy + cyDelta;
 
         this.buildNode(
             curTree,
-            sonLeft,
-            radius - 1,
-            cx - cxDelta,
+            sonLeftId,
+            radius,
+            cxSonLeft,
             cxDelta / 2,
-            cy + cyDelta,
+            cySonLeft,
             cyDelta);
+
+        this.connectNodes(nodeId, sonLeftId);
       }
 
-      if (sonRight >= 0 && sonRight < curTree.capacity) {
-        this.links.append('line')
-          .attr('class', 'link')
-          .attr('x1', cx)
-          .attr('y1', cy)
-          .attr('x2', cx + cxDelta)
-          .attr('y2', cy + cyDelta)
-          .style('stroke', 'rgb(64,64,64)')
-          .style('stroke-width', 4);
+      if (sonRightId >= 0 && sonRightId < curTree.capacity) {
+        const cxSonRight = cx + cxDelta;
+        const cySonRight = cy + cyDelta;
 
         this.buildNode(
             curTree,
-            sonRight,
-            radius - 1,
-            cx + cxDelta,
+            sonRightId,
+            radius,
+            cxSonRight,
             cxDelta / 2,
-            cy + cyDelta,
+            cySonRight,
             cyDelta);
+
+        this.connectNodes(nodeId, sonRightId);
       }
   }
 
