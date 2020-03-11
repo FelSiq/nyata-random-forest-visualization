89e22581161e56524c5fecd69e220a98261641f1
felsiq <felipe.siqueira@usp.br>
2020-03-11 22:28:19 +0200

    Sketch to get obj class documentation descriptions

--- backend/model_dt/descriptions.py
+++ backend/model_dt/descriptions.py
@@ -1,5 +1,39 @@
 """Solve item descriptions."""
 import typing as t
+import re
+
+import sklearn.tree
+import sklearn.ensemble
+
+
+def build_class_arg_key(obj, prefix: str = "class_") -> str:
+    """Build a standard description key for a given object attribute."""
+    return prefix + str(type(obj))
+
+
+def get_class_doc(model, prefix: str = "class_") -> t.Dict[str, str]:
+    """Get documentation of given class instantiation arguments and attributes."""
+    preproc = re.sub("\n\n|--+", "__@", model.__doc__)
+    preproc = re.sub("([^\s]) : ([^\n]+)", r"\1 : (Type: \2)", preproc)
+    preproc = re.sub("\s+", " ", preproc)
+    preproc = re.sub("(?<=__@)\s", "", preproc)
+    preproc = re.sub("``|`", '"', preproc)
+    preproc = preproc.split("__@")
+
+    res = {}  # type: t.Dict[str, str]
+
+    full_prefix = build_class_arg_key(obj=model, prefix=prefix)
+
+    for piece in preproc:
+        try:
+            param, desc = piece.split(" : ")
+            res[full_prefix + param] = desc
+
+        except ValueError:
+            pass
+
+    return res
+
 
 DESCRIPTIONS = {
     "within_cluster_dists":
@@ -12,10 +46,17 @@ DESCRIPTIONS = {
     "...",
     "feature_importances":
     "...",
-    "prediction_result": "...",
-    "classes_by_tree": "...",
-    "pred_margin": "Margin is the instance highest class probability "
-                   "minus second highest class probability.",
+    "prediction_result":
+    "...",
+    "classes_by_tree":
+    "...",
+    "pred_margin":
+    "Margin is the instance highest class probability "
+    "minus second highest class probability.",
+    **get_class_doc(sklearn.ensemble.RandomForestClassifier),
+    **get_class_doc(sklearn.ensemble.RandomForestRegressor),
+    **get_class_doc(sklearn.tree.DecisionTreeClassifier),
+    **get_class_doc(sklearn.tree.DecisionTreeRegressor),
 }  # t.Dict[t.Any, str]
 
 
@@ -31,10 +72,17 @@ def dictionarize(value: t.Any, auxiliar_value: t.Any,
 
 def add_desc(value: t.Any,
              desc: t.Optional[str] = None,
-             from_id: t.Optional[t.Any] = None) -> t.Dict[str, t.Any]:
+             from_id: t.Optional[t.Any] = None,
+             from_obj_doc: t.Optional[t.Any] = None) -> t.Dict[str, t.Any]:
     """Add a description to an object."""
+    if from_obj_doc is not None and from_id is None:
+        raise ValueError("'from_id' must be given if 'from_obj_doc' is given.")
+
     if desc is None:
-        desc = DESCRIPTIONS.get(from_id, "TODO.")
+        if from_obj_doc is not None:
+            from_id = build_class_arg_key(obj=from_obj_doc) + from_id
+
+        desc = DESCRIPTIONS.get(from_id, None)
 
     return dictionarize(value=value,
                         auxiliar_value=desc,
