4f012311eb12202cdf07cb536df60102452bae52
felsiq <felipe.siqueira@usp.br>
2020-03-12 17:51:24 -0300

    server config #2

--- backend/model_dt/__init__.py
+++ backend/model_dt/__init__.py
@@ -343,7 +343,6 @@ class Config:
         os.environ.get("SESSION_REDIS", "redis://127.0.0.1:6379"))
     SESSION_USE_SIGNER = os.environ.get("SESSION_USE_SIGNER", False)
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SESSION_COOKIE_NAME = os.environ.get("SESSION_COOKIE_NAME")
     SESSION_COOKIE_SECURE = bool(int(os.environ.get("SESSION_COOKIE_SECURE", False)))
     SESSION_PERMANENT = bool(int(os.environ.get("SESSION_PERMANENT", True)))
     PERMANENT_SESSION_LIFETIME = int(os.environ.get("PERMANENT_SESSION_LIFETIME", 60 * 60 * 2))
