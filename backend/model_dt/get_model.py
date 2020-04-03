b483978e58547297269f761bdb07ec8a54ee6e10
felsiq <felipe.siqueira@usp.br>
2020-04-03 20:52:12 +0300

    Using toy model again

--- backend/model_dt/get_model.py
+++ backend/model_dt/get_model.py
@@ -35,37 +35,4 @@ def get_toy_model(forest: bool = True, regressor: bool = False):
 
 
 def get_custom_model():
-    import pandas as pd
-    import numpy as np
-    from sklearn.ensemble import RandomForestClassifier
-    
-    X_data = np.loadtxt(
-        './custom_model/dados_hospital_cancer/X_train.txt')
-    y_data = pd.Series(
-        np.loadtxt('./custom_model/dados_hospital_cancer/y_train.txt'), name = 'pseudost', dtype = int)
-    
-    att_labels = np.loadtxt('./custom_model/dados_hospital_cancer/Attributes.txt', dtype='str')
-    att_freq5 = np.loadtxt('./custom_model/dados_hospital_cancer/att_freq5', dtype='int')
-    att_freq6 = np.loadtxt('./custom_model/dados_hospital_cancer/att_freq6', dtype='int')
-    
-    estimators=50; criterion='entropy'; max_features='auto'
-    
-    """
-    # Fequency = 5
-    # print(att_labels[att_freq5==1])
-    X_fitness5 = np.delete(X_data, np.where(att_freq5 == 0)[0], axis=1)
-    model_freq5 = RandomForestClassifier(n_estimators=estimators, max_features=max_features, 
-                                         criterion=criterion, random_state=0)
-    model_freq5.fit(X_fitness5, y_data)
-
-    return model_freq5, X_fitness5, y_data, att_labels[att_freq5==1]
-    """
-    
-    # Fequency = 6
-    # print(att_labels[att_freq6==1])
-    X_fitness6 = np.delete(X_data, np.where(att_freq6 == 0)[0], axis=1)
-    model_freq6 = RandomForestClassifier(n_estimators=estimators, max_features=max_features, 
-                                         criterion=criterion, random_state=0)
-    model_freq6.fit(X_fitness6, y_data)
-
-    return model_freq6, X_fitness6, y_data, att_labels[att_freq6==1]
+    pass
