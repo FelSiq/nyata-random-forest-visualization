0660511a20340dadc9d9632bf6f835902eeeb167
felsiq <felipe.siqueira@usp.br>
2020-04-03 18:33:23 +0300

    Added save as .pdf for tree analysis tab

--- frontend/model-dt/src/app/dt-model/tree-displayer/forest-analysis/forest-analysis.component.ts
+++ frontend/model-dt/src/app/dt-model/tree-displayer/forest-analysis/forest-analysis.component.ts
@@ -5,6 +5,10 @@ import { Input, Output, EventEmitter } from '@angular/core';
 import { HierClus, ClusterNode, ClusterData, KeyValuePair } from './hier-clus';
 import { Subject } from 'rxjs/Subject';
 
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+
+
 @Component({
   selector: 'app-forest-analysis',
   templateUrl: './forest-analysis.component.html',
@@ -201,5 +205,23 @@ export class ForestAnalysisComponent implements OnInit, OnDestroy {
           this.errorMessage = 'Something went wrong while communicating with the backend.';
           this.calledHierClusCutService = false;
         });
+  }
+
+  captureScreen(id: string, pdfName: string = 'analysis-data.pdf'): void {
+    const data = document.getElementById(id);  
+
+    html2canvas(data).then(canvas => {
+      const origRatio = canvas.width / canvas.height;
+
+      // Note 1px ~= 0.2645833333333mm
+      const imgHeight = Math.min(290, canvas.height * 0.264583);
+      const imgWidth = Math.min(200, canvas.width * 0.264583, imgHeight * origRatio);
+  
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4');
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save(pdfName);
+    });
   }
 }
--- frontend/model-dt/src/app/dt-model/tree-displayer/iterable-displayer/iterable-displayer.component.ts
+++ frontend/model-dt/src/app/dt-model/tree-displayer/iterable-displayer/iterable-displayer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import * as jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
@@ -50,18 +50,20 @@ export class IterableDisplayerComponent implements OnInit {
     );
   }
 
-  public captureScreen(id: string, pdfName: string = 'item.pdf'): void {
+  captureScreen(id: string, pdfName: string = 'item.pdf'): void {
     const data = document.getElementById(id);  
 
     html2canvas(data).then(canvas => {
+      const origRatio = canvas.width / canvas.height;
+
       // Note 1px ~= 0.2645833333333mm
-      const imgWidth = Math.min(200, canvas.width * 0.264583);
       const imgHeight = Math.min(290, canvas.height * 0.264583);
+      const imgWidth = Math.min(200, canvas.width * 0.264583, imgHeight * origRatio);
   
-      const contentDataURL = canvas.toDataURL('image/png')
+      const contentDataURL = canvas.toDataURL('image/png');
       const pdf = new jspdf('p', 'mm', 'a4');
       const position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
       pdf.save(pdfName);
     });
   }
