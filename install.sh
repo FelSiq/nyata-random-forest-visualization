254220442a895a0e641c7dac81e721784ded791d
felsiq <felipe.siqueira@usp.br>
2021-07-15 12:11:00 -0300

    Improved install.sh script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
-if [ $UID > 0 ]
+if [[ $UID > 0 ]]
 then
 	echo "Warning: will may want to run this script in super-user mode."
 fi
 
+if [[ $# -eq 0 ]]
+then
+	echo "Please provide your package manager: 'apt', 'apt-get', 'yay' or 'pacman'."
+	exit 1
+fi
+
 cd ./backend
-make install
+make install-"$1"
 cd ../frontend/model-dt
 npm install
 npm audit fix
